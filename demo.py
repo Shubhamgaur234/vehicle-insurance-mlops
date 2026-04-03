eb0ec5559e2e273412c869f3662ab60c586cb38d
shubham <gaur3786@gmal.com>
2026-04-03 04:41:01 -0700

    step-1 done

---
+++ demo.py
@@ -0,0 +1,13 @@
+from src.logger import logging
+from src.exception import MyException
+import sys
+logging.debug("This is a debug message.")
+
+
+
+
+try:
+    a = 1+'Z'
+except Exception as e:
+     logging.info(e)
+     raise MyException(e, sys) from e
