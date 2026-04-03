cc256b7ba970b0caa18354033221fc9868a09731
shubham <gaur3786@gmal.com>
2026-04-03 10:34:46 -0700

    data ingestion

--- demo.py
+++ demo.py
@@ -1,13 +1,18 @@
-from src.logger import logging
-from src.exception import MyException
-import sys
-logging.debug("This is a debug message.")
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+# logging.debug("This is a debug message.")
 
 
 
 
-try:
-    a = 1+'Z'
-except Exception as e:
-     logging.info(e)
-     raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#      logging.info(e)
+#      raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
